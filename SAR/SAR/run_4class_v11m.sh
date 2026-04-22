62baf0c1465867083a687a55a8259c7c0af8ff15
Your Name <you@example.com>
2026-04-22 10:56:53 +0000

    Clean up repository: Remove old/redundant scripts, keep only ResNet50RT and YOLO New Gen

--- SAR/SAR/run_4class_v11m.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Pastikan library di folder .deps dan .deps_yolo terdeteksi
-export PYTHONPATH=../../.deps_yolo:../../.deps
-
-# Jalankan training YOLOv11m 4 Class
-python3 train_yolo_v11m_4class.py
--- SAR/SAR/run_train.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=../../.deps_yolo:../../.deps
-python3 train_yolo_v11m.py
